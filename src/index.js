b4b4e3059330004e3218f426c09938be23d5384b
MarcinChorzepa <mchorzepa1@tlen.pl>
2019-05-01 12:51:09 +0200

    all done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends Component{
+    constructor(props){
+        super(props);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    }
+    render(){
+        const buttonText = "Click me!"
+        return (
+            <div>
+                <label htmlFor="name" className="label">Enter name:</label>
+                <input id="name" type="text"/>
+                <button style={{backgroundColor: 'blue',color: 'white'}}>
+                    {buttonText}
+                </button>
+                </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />,
+    document.querySelector('#root')
+);
