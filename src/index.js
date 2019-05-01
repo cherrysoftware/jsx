88bf9496593236cb4cf63ebda531bb9c23872b2f
MarcinChorzepa <mchorzepa1@tlen.pl>
2019-05-01 20:24:10 +0200

    function components

--- src/index.js
+++ src/index.js
@@ -1,22 +1,31 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 class App extends Component{
     constructor(props){
         super(props);
 
     }
     render(){
-        const buttonText = "Click me!"
         return (
-            <div>
-                <label htmlFor="name" className="label">Enter name:</label>
-                <input id="name" type="text"/>
-                <button style={{backgroundColor: 'blue',color: 'white'}}>
-                    {buttonText}
-                </button>
-                </div>
-        )
+            <div className="ui container comments">
+                <ApprovalCard>
+                    <h4>Warning!!!</h4>
+                    <div>Are you sure you want to do this?</div>
+                </ApprovalCard>
+                <ApprovalCard>
+               <CommentDetail author="Sam" date="6:00 PM" blogText="Nice blog post!!!" avatar={faker.image.avatar()}/>
+                </ApprovalCard>
+                <ApprovalCard>
+                <CommentDetail author="Monika" date="11:00 PM" blogText="No it is not fine!!!" avatar={faker.image.avatar()}/>
+                </ApprovalCard>
+                    <ApprovalCard>
+                    <CommentDetail author="Zorro" date="5:00 PM" blogText="Long text for purpose of limit the space" avatar={faker.image.avatar()}/>
+                    </ApprovalCard>
+            </div>
+        );
     }
 }
 
